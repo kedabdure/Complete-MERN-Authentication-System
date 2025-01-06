4aa1f7e2900021364f7a5abe6c26e9182532a841
kedabudre <abdurehimk77@gmail.com>
2025-01-06 03:46:10 +0300

    create a cotroller function for user registration

---
+++ server/controllers/authController.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/user.model';
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ message: "All fields are required", success: false });
+  }
+
+  try {
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      name, email, password: hashedPassword
+    })
+    await user.save();
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+      expiresIn: '7d'
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    })
+
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+
+}
