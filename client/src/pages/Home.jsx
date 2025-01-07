df1409dcbe2fd9f2e4e83aa28668af8e307b1a44
kedabudre <abdurehimk77@gmail.com>
2025-01-08 01:15:50 +0300

    update the user interface using tailwind css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header"
 
 const home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
       <Navbar />
       <Header />
     </div>
