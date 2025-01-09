56ffde90dd299a2ae7f951bb8f50d3c2ca25349a
kedabudre <abdurehimk77@gmail.com>
2025-01-09 11:27:32 +0300

    add a passwrod reset functionality for the app

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,8 +1,177 @@
+import { useState, useRef, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { assets } from "../assets/assets"
+import { AppContext } from "../context/AppContext"
+import axios from "axios"
+import { toast } from "react-toastify"
+
 const ResetPassword = () => {
+  const inputRefs = useRef([]);
+  const [email, setEmail] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [otp, setOtp] = useState(0)
+  const [isEmailSent, setIsEmailSent] = useState('')
+  const [isOtpSubmitted, setIsOtpSubmitted] = useState(false)
+
+  axios.defaults.withCredentials = true;
+  const { backendUrl } = useContext(AppContext)
+  const navigate = useNavigate()
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  }
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    })
+  }
+
+  const onSubmitEmail = async (e) => {
+    e.preventDefault()
+
+    try {
+      const { data } = await axios.post(backendUrl + '/api/auth/send-reset-otp', { email })
+      if (data.success) {
+        setIsEmailSent(true)
+
+        toast.success(data.message)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const onSubmitOtp = async (e) => {
+    e.preventDefault()
+
+    const otpArray = inputRefs.current.map(e => e.value)
+    setOtp(otpArray.join(""))
+    setIsOtpSubmitted(true)
+  }
+
+  const onSubmitNewPassword = async (e) => {
+    e.preventDefault()
+
+    try {
+      const { data } = await axios.post(backendUrl + '/api/auth/reset-password', { email, otp, newPassword })
+      data.success ? toast.success(data.message) : toast.error(data.message)
+      data.success && navigate('/login')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>
-      <h1>Reset password</h1>
-    </div>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+
+      {/* Reset Password */}
+      {!isEmailSent && (
+        <form onSubmit={onSubmitEmail} className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+          <h1 className="text-white text-2xl font-semibold text-center mb-4">
+            Reset Password
+          </h1>
+          <p className="text-center mb-6 text-indigo-300">
+            Enter your registered email address.
+          </p>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]  ">
+            <img src={assets.mail_icon} alt="mail-icon" className="w-4 h-4" />
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              required
+              className="bg-transparent outline-none text-white" />
+          </div>
+
+          <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3">Submit</button>
+        </form>
+      )}
+
+
+      {/* Enter OTP */}
+      {!isOtpSubmitted && isEmailSent && (
+        < form onSubmit={onSubmitOtp} className="bg-slate-900 p-8 rounded-lg shadow-lg text-sm">
+          <h1 className="text-white text-2xl font-semibold text-center mb-4">
+            Reset password OTP
+          </h1>
+          <p className="text-center mb-6 text-indigo-300">
+            Enter the 6-digit code sent to your email id.
+          </p>
+
+          <div className="flex justify-between mb-8 gap-2" onPaste={handlePaste}>
+            {Array(6)
+              .fill(0)
+              .map((_, index) => (
+                <input
+                  type="text"
+                  maxLength={1}
+                  key={index}
+                  required
+                  className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                  ref={(el) => (inputRefs.current[index] = el)}
+                  onInput={(e) => handleInput(e, index)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                />
+              ))}
+          </div>
+          <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+            Submit
+          </button>
+        </form>
+      )
+      }
+
+
+      {/* Enter new password */}
+      {isOtpSubmitted && isEmailSent && (
+        < form onSubmit={onSubmitNewPassword} className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+          <h1 className="text-white text-2xl font-semibold text-center mb-4">
+            New Password
+          </h1>
+          <p className="text-center mb-6 text-indigo-300">
+            Enter the new password bellow.
+          </p>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]  ">
+            <img src={assets.lock_icon} alt="mail-icon" className="w-4 h-4" />
+            <input
+              type="password"
+              placeholder="New Password"
+              value={newPassword}
+              onChange={e => setNewPassword(e.target.value)}
+              required
+              className="bg-transparent outline-none text-white" />
+          </div>
+
+          <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3">Submit</button>
+        </form>
+      )
+      }
+
+    </div >
   )
 }
 
