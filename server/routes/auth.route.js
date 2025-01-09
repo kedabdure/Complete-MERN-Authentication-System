dad21e7471bf05840ebba7410f0c1ef689be45b8
kedabudre <abdurehimk77@gmail.com>
2025-01-09 10:06:49 +0300

    updat the auth route for to verify-account

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -8,8 +8,8 @@ authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
-authRouter.post('/verify-email', userAuth, verifyEmail);
-authRouter.post('/is-auth', userAuth, isAuthenticated);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.get('/is-auth', userAuth, isAuthenticated);
 authRouter.post('/send-reset-otp', sendResetOtp);
 authRouter.post('/reset-password', resetPassword);
 
